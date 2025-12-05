4c412c84b205475e0e31df9a6694812c17b10d8a
Tomax-21 <thom21fr@gmail.com>
2025-12-05 14:21:50 +0100

    initialisation de la classe product

--- Product.cpp
+++ Product.cpp
@@ -1,1 +1,12 @@
 #include "Product.h"
+
+Product::Product(std::string const& title, std::string const& description, int const& quantity, float const& unit_price):
+	title_(title), description_(description), quantity_(quantity), unit_price_(unit_price)
+{
+	if (quantity < 0) {
+		throw std::invalid_argument("La quantité doit etre une valeur positive ou nulle");
+	}
+	if (unit_price < 0) {
+		throw std::invalid_argument("Le prix unitaire doit etre une valeur positive ou nulle");
+	}
+}
--- Product.h
+++ Product.h
@@ -1,5 +1,20 @@
 #pragma once
+
+#include <string>
+#include <iostream>
+
 class Product
 {
+private:
+	std::string id_;
+	std::string title_;
+	std::string description_;
+	int quantity_;
+	float unit_price_;
+
+
+public:
+	Product(std::string const& title, std::string const& description, int const& quantity, float const& unit_price);
+
 };
 
